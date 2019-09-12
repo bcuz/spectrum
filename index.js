145af88f1ccd84f2d063a61900b5741bb18aef54
LPascholatti <lucaspascholatti@gmail.com>
2019-09-12 13:18:23 +0200

    implemented new questions, initial card and buttons, flip card & flip back in English

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 let flashcards = [
-    { id: 1, question: 'good afternoon', answer: 'goedemiddag' },
-    { id: 2, question: 'bicycle', answer: 'fiets' },
-    { id: 3, question: 'vacuum cleaner', answer: 'stofzuiger' },
+  { id: 1, question: 'Good Afternoon', answer: 'Goedemiddag' },
+  { id: 2, question: 'Bicycle', answer: 'Fiets' },
+  { id: 3, question: 'Vacuum Cleaner', answer: 'Stofzuiger' },
+  { id: 4, question: 'Strong', answer: 'Sterk'},
+  { id: 5, question: 'Backpack', answer: 'Rugzak'},
+  { id: 6, question: 'Guitar', answer: 'Gitar'}
 ]
 
 let randomCardNumber = Math.floor(Math.random() * flashcards.length)
+console.log("Random", randomCardNumber)
 
-console.log(randomCardNumber)
+const word = document.getElementById("word")
+const button = document.getElementById("button")
+
+console.log(word)
+console.log(flashcards[1].question)
+console.log(button)
+
+word.innerText = flashcards[randomCardNumber].question
+
+function returnAnswer() {
+    return word.innerText = flashcards[randomCardNumber].answer
+}
+
+function flipBack() {
+    return word.innerText = flashcards[randomCardNumber].question
+}
 
